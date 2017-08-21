7f40bb92ab3fd54b105048ec467b974013bc6ce4
Valentin Mihov <valentin.mihov@gmail.com>
2017-08-21 11:56:30 +0300

    Remove the isotope filtering Plugin
    Thius plugin is causing layout problem, so removing it.

--- js/custom.js
+++ js/custom.js
@@ -60,6 +60,9 @@ $("#menu > a").click(function () {
 
 (function ($) {
 $('#menu > a').bind('click', function () {
+	console.log("Test");
+	$('#menu #nav').hide();
+
   if ($(this).hasClass('current')) {
 	  $(this).removeClass('current');
 	  $(this).parent().parent().find('#menu > ul').slideUp('fast');
@@ -173,74 +176,6 @@ if (text.is(':hidden')) {
 	$(this).siblings('span').html('+');
 }
 });
-
-
-// Isotope Filtering
-$(function(){
-
-var $container = $('#contain');
-
-// initialize Isotope
-  $container.isotope({
-	// options...
-	resizable: false, // disable normal resizing
-	// set columnWidth to a percentage of container width
-	masonry: { columnWidth: $container.width() / 12 }
-  });
-
-  // update columnWidth on window resize
-  $(window).smartresize(function(){
-	$container.isotope({
-	  // update columnWidth to a percentage of container width
-	  masonry: { columnWidth: $container.width() / 12 }
-	});
-  });
-
-
-$container.isotope({
-  itemSelector : '.item',
-   animationOptions: {
-     duration: 750,
-     easing: 'linear',
-     queue: true
-   }
-});
-
-
-var $optionSets = $('#options .option-set'),
-	$optionLinks = $optionSets.find('a');
-
-$optionLinks.click(function(){
-  var $this = $(this);
-  // don't proceed if already selected
-  if ( $this.hasClass('selected') ) {
-	return false;
-  }
-  var $optionSet = $this.parents('.option-set');
-  $optionSet.find('.selected').removeClass('selected');
-  $this.addClass('selected');
-
-  // make option object dynamically, i.e. { filter: '.my-filter-class' }
-  var options = {},
-	  key = $optionSet.attr('data-option-key'),
-	  value = $this.attr('data-option-value');
-  // parse 'false' as false boolean
-  value = value === 'false' ? false : value;
-  options[ key ] = value;
-  if ( key === 'layoutMode' && typeof changeLayoutMode === 'function' ) {
-	// changes in layout modes need extra logic
-	changeLayoutMode( $this, options )
-  } else {
-	// otherwise, apply new options
-	$container.isotope( options );
-  }
-
-  return false;
-});
-
-
-});
-
 
 /* FancyBox plugin */
 $('.fancybox').fancybox({
